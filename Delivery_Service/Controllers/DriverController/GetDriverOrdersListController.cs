830b6725bb61a40aec2d1b52c21dc98a9c2a6998
Abdullah-Alhamdani <55024863+Abdullah-Alhamdani@users.noreply.github.com>
2024-04-21 01:07:16 +0300

    test: add Integration Test to the customer service

---
+++ Delivery_Service/Controllers/DriverController/GetDriverOrdersListController.cs
@@ -0,0 +1,39 @@
+﻿using Delivery_Service.Application.Dto.Driver;
+using Delivery_Service.Domain;
+using DeliveryService.Application.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery_Service.Controllers.DriverController
+{
+    [Route("api/GetDriverOrdersList")]
+    [ApiController]
+    public class GetDriverOrdersListController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetDriverOrdersListController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// This API Allowing the driver to view the current orders he has.
+        /// </summary>
+        /// <param name="driverId"></param>         
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] string driverId)
+        {
+            try
+            {
+                List<ViewDriverOrderDto> viewDriverOrderDtos = await _unitOfWork.GetDriverOrdersListAsync(driverId);               
+                
+                return Ok(viewDriverOrderDtos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+    }
+}
